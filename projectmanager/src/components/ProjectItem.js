a4b8104d9eb32126c49bebc1b53c228844e29d48
LeeroyHannigan <leeroyhannigan@yahoo.ie>
2018-06-04 20:04:49 +0100

    first commit

---
+++ projectmanager/src/components/ProjectItem.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class ProjectItem extends Component {
+  deleteProject(id){
+   this.props.onDelete(id)
+  }
+  render()  {
+      console.log(this.props);
+    return (
+      <li className="Project">
+    <strong>{this.props.project.title}</strong>: {this.props.project.category} <button  onClick={this.deleteProject.bind(this,this.props.project.id)}> X </button>
+     
+      </li>
+    );
+  }
+}
+
+export default ProjectItem;
